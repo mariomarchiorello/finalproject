ce2e77221fa7768c3e53f9060a28e665a1854547
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-04-08 11:09:56 +0000

    Update frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = ()=> {
   return (
     <>
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/signIn-signUp' component={XXX} />
-        <Route exact path='/map' component={Map} />
-        <Route exact path='/annotate' component={XXX} />
-        <Route exact path='/profile' component={XXX} />
+    // <Router>
+    //   <Switch>
+    //     <Route exact path='/' component={Home} />
+    //     <Route exact path='/signIn-signUp' component={XXX} />
+    //     <Route exact path='/map' component={Map} />
+    //     <Route exact path='/annotate' component={XXX} />
+    //     <Route exact path='/profile' component={XXX} />
 
 
 
 
         
 
-      </Switch>
-    </Router>
+    //   </Switch>
+    // </Router>
     
     </>
   );
