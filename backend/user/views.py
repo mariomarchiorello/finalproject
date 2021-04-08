033069c796eb7f9b7964138f7e8809be2865a3a1
Dusko <radovic_dusko@yahoo.com>
2021-04-08 23:15:59 +0200

    user endpoints

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from backend.user.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class GetMyProfile(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
