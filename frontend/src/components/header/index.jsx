7820386df92a0a5a9ce31d5ebdc3cea9f096a8ab
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-04-13 19:01:41 +0200

    profile paeges

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -3,12 +3,18 @@ import { HeaderContainer, Logo, RightContainer, Profile, LoginButton, LogoContai
 
 import {withAuth} from "../HOC";
 import {Link} from "react-router-dom";
+import {useDispatch} from "react-redux";
 
 const Header = () => {
 
+    const dispatch = useDispatch();
+
+    const profileHandler = value => {
+        dispatch({type:"PROFILE-EDIT-HANDLER",payload: value})
+        console.log("from in da header--> go to profile")
+    };
 
     const logOut = () => localStorage.clear()
-
 
     return  <>
     <HeaderContainer>
@@ -16,7 +22,7 @@ const Header = () => {
             <LogoContainer>
                 <Logo src={first}/>
             </LogoContainer>
-            { withAuth( <RightContainer><Profile to='/profile'>Username</Profile><LoginButton onCLick={logOut}>Sign Out</LoginButton></RightContainer> )}
+            { withAuth( <RightContainer><Profile onClick={()=>profileHandler("info")} to='/profile'>Username</Profile><LoginButton onCLick={logOut}>Sign Out</LoginButton></RightContainer> )}
             <RightContainer><Link to='/sign-up'><Profile>Join</Profile></Link><Link to = '/sign-in'><LoginButton >Sign in</LoginButton></Link></RightContainer>;
 
     </HeaderContainer>
--- frontend/src/components/profile/self/index.jsx
+++ frontend/src/components/profile/self/index.jsx
@@ -1,20 +1,44 @@
 import React from 'react'
-import ButtonSection from './buttonSection'
 import EditPasswordSection from './editPassword'
 import EditProfileSection from './editProfile'
 import InfoSection from './info'
-import { ProfileCenterContainer } from './style'
+import { ProfileCenterContainer} from './style'
+import {SmallBlueButton} from "../../../globalstyles/globalStyle";
+import {BottomContainer} from "../style";
+import {useDispatch, useSelector} from "react-redux";
 
 export default function MyProfile() {
+
+    const dispatch = useDispatch()
+
+    const profileHandler = value => { dispatch({type:"PROFILE-EDIT-HANDLER",payload:value})}
+
+
+
+     const profileEdit = useSelector( state => state.profileEdit.profileEditSection);
+
+
+
+
     return (
         <>
+            <div>
             <ProfileCenterContainer>
-                <InfoSection></InfoSection>
-                <EditProfileSection></EditProfileSection>
-                <EditPasswordSection></EditPasswordSection>
-                <ButtonSection></ButtonSection>
+
+                 <InfoSection/>
+                    <BottomContainer>
+                    <SmallBlueButton onClick={()=>profileHandler("editProfile")} >Edit</SmallBlueButton>
+                    </BottomContainer>
+
+
+                {/*<EditProfileSection/>*/}
+
+                {/*<EditPasswordSection/>*/}
+
+
 
             </ProfileCenterContainer>
+            </div>
             
         </>
     )
