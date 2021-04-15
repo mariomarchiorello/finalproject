e8c17f9c2c72f617435378c38b45bad3a8d48bac
zequi <calvo.ezequias@gmail.com>
2021-04-15 13:14:37 -0300

    canvas integration

---
+++ frontend/src/components/canvasContainer/CanvasContext.jsx
@@ -0,0 +1,103 @@
+import React, { useContext, useRef, useState, useEffect } from "react";
+import canvasToImage from "canvas-to-image";
+
+const CanvasContext = React.createContext();
+
+export const CanvasProvider = ({ children }) => {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [image, setImage] = useState(null);
+  const [counter, setCounter] = useState(0);
+
+  const prepareCanvas = () => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth / 1.5;
+    canvas.height = window.innerHeight / 1.5;
+    canvas.style.width = `${window.innerWidth / 3}px`;
+    canvas.style.height = `${window.innerHeight / 3}px`;
+
+    const context = canvas.getContext("2d");
+
+    context.scale(2, 2);
+    context.strokeStyle = "#98FB98"; //{color}
+    context.lineWidth = 5;
+    contextRef.current = context;
+  };
+
+  const startDrawing = () => {
+    contextRef.current.beginPath();
+    setIsDrawing(true);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.arc(offsetX, offsetY, 20, 0, Math.PI * 2);
+    contextRef.current.stroke();
+    contextRef.current.closePath();
+    setIsDrawing(false);
+    setCounter((counter) => (counter += 1));
+    console.log(contextRef.current, "context ref");
+    console.log(canvasRef.current, "canvas ref");
+    // const test = canvasToImage(canvasRef.current, {
+    //   name: "myImage",
+    //   type: "jpg",
+    //   quality: 0.7,
+    // });
+
+    // console.log(test, "test");
+  };
+
+  const clearCanvas = () => {
+    setCounter((counter) => (counter = 0));
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // (image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+  };
+
+  const displayImage = () => {
+    console.log(canvasRef.current.width);
+    contextRef.current.drawImage(
+      image,
+      0,
+      0,
+      canvasRef.current.width / 2,
+      canvasRef.current.height / 2
+    );
+  };
+
+  useEffect(() => {
+    const catImage = new Image();
+    catImage.crossorigin = "anonymous";
+    catImage.src = "https://thiscatdoesnotexist.com/";
+    setImage(catImage);
+  }, []);
+
+  return (
+    <>
+      <CanvasContext.Provider
+        value={{
+          canvasRef,
+          contextRef,
+          prepareCanvas,
+          startDrawing,
+          // finishDrawing,
+          displayImage,
+          clearCanvas,
+          draw,
+        }}
+      >
+        {children}
+      </CanvasContext.Provider>
+      <div>Plankton: {counter}</div>
+      {/* <button onClick={saveImage}></button> */}
+    </>
+  );
+};
+
+export const useCanvas = () => useContext(CanvasContext);
