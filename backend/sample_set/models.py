dc920a1b0d933dd11ef91fb7b8ea095d92c67c7c
Dusko <radovic_dusko@yahoo.com>
2021-04-15 23:27:06 +0200

    new and modified endpoints

--- backend/sample_set/models.py
+++ backend/sample_set/models.py
@@ -1,19 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-WATER_SURFACE = [
-    ('O', 'Oily surface'),
-    ('F', 'Foamy surface'),
-    ('B', 'Bioluminescent')
-]
-
-WEATHER_CONDITIONS = [
-    ('Storm', 'Stormy'),
-    ('Rain', 'Rainy'),
-    ('Sun', 'Sunny'),
-    ('Cloud', 'Cloudy'),
-    ('Snow', 'Snowy')
-]
+# WATER_SURFACE = [
+#     ('Oil', 'Oily surface'),
+#     ('Foam', 'Foamy surface'),
+#     ('Bioluminescence', 'Bioluminescent')
+# ]
+#
+# WEATHER_CONDITIONS = [
+#     ('Storm', 'Stormy'),
+#     ('Rain', 'Rainy'),
+#     ('Sun', 'Sunny'),
+#     ('Cloud', 'Cloudy'),
+#     ('Snow', 'Snowy')
+# ]
 
 User = get_user_model()
 
@@ -33,13 +33,21 @@ class SampleSet(models.Model):
                                      null=True,
                                      related_name='sample_sets')
     is_completed = models.BooleanField(default=False)
+    water_oil = models.BooleanField(null=True, default=False)
+    water_foam = models.BooleanField(null=True, default=False)
+    water_bioluminescence = models.BooleanField(null=True, default=False)
+    weather_storm = models.BooleanField(null=True, default=False)
+    weather_rain = models.BooleanField(null=True, default=False)
+    weather_sun = models.BooleanField(null=True, default=False)
+    weather_cloud = models.BooleanField(null=True, default=False)
+    weather_snow = models.BooleanField(null=True, default=False)
 
 
-class WaterSurface(models.Model):
-    property = models.CharField(max_length=10, choices=WATER_SURFACE, blank=True)
-    sample = models.ForeignKey(to=SampleSet, related_name='water_surface', on_delete=models.CASCADE)
-
-
-class WeatherCondition(models.Model):
-    property = models.CharField(max_length=6, choices=WEATHER_CONDITIONS, blank=True)
-    sample = models.ForeignKey(to=SampleSet, related_name='weather_conditions', on_delete=models.CASCADE)
+# class WaterSurface(models.Model):
+#     property = models.CharField(max_length=20, choices=WATER_SURFACE, blank=True)
+#     sample = models.ForeignKey(to=SampleSet, related_name='water_surface', on_delete=models.CASCADE)
+#
+#
+# class WeatherCondition(models.Model):
+#     property = models.CharField(max_length=6, choices=WEATHER_CONDITIONS, blank=True)
+#     sample = models.ForeignKey(to=SampleSet, related_name='weather_conditions', on_delete=models.CASCADE)
--- backend/sample_set/serializers/default.py
+++ backend/sample_set/serializers/default.py
@@ -1,14 +1,13 @@
 from rest_framework import serializers
 
-from image.models import Image
 from image.serializers import ImageSerializer
 from sample_set.models import SampleSet
-from sample_set.serializers.nested import WaterSurfaceSerializer
 
 
 class SampleSetSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True)
-    water_surface = WaterSurfaceSerializer(read_only=True, many=True)
+    # water_surface = WaterSurfaceSerializer(read_only=True, many=True)
+    # weather_conditions = WeatherSerializer(read_only=True, many=True)
 
     class Meta:
         model = SampleSet
