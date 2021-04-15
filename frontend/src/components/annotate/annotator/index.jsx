1a48dc288b228252db4d23250fd99bdbe41b95e9
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-04-16 00:59:22 +0200

    updated jason lock and npm

--- frontend/src/components/annotate/annotator/index.jsx
+++ frontend/src/components/annotate/annotator/index.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactImageAnnotate from "react-image-annotate";
-import imageTwo from '../../../assets/background-images/plankton.jpg'
-
-const Annotator = () => (
-  <ReactImageAnnotate
-    labelImages
-    regionClsList={["Zoo", "Phyto"]}
-    images={[
-      {
-        src: "https://placekitten.com/408/287",
-        name: "Image 1",
-        regions: []
-      },
-      {
-        src: imageTwo,
-        name: "Image 2",
-        regions: []
-      }
-    ]}
-  />
-);
-
-export default Annotator;
+// import React from "react";
+// import ReactImageAnnotate from "react-image-annotate";
+// import imageTwo from '../../../assets/background-images/plankton.jpg'
+//
+// const Annotator = () => (
+//   <ReactImageAnnotate
+//     labelImages
+//     regionClsList={["Zoo", "Phyto"]}
+//     images={[
+//       {
+//         src: "https://placekitten.com/408/287",
+//         name: "Image 1",
+//         regions: []
+//       },
+//       {
+//         src: imageTwo,
+//         name: "Image 2",
+//         regions: []
+//       }
+//     ]}
+//   />
+// );
+//
+// export default Annotator;
 
