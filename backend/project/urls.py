7c5fe643628f1295b888c93dfca3e9695e550f95
Dusko <radovic_dusko@yahoo.com>
2021-04-19 16:07:04 +0200

    canvas trials

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
@@ -45,3 +47,7 @@ urlpatterns = [
     path('backend/admin/', admin.site.urls),
     path('backend/api/', include(api_patterns)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
